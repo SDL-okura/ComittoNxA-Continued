ee0c39bd75989186dc55da5f71c4fda8719a89d1
yuma211-lab <78386834+yuma211-lab@users.noreply.github.com>
2021-11-14 00:29:34 +0900

    Delete some makefiles

--- app/src/main/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI := armeabi-v7a x86
-#APP_ABI := x86
-#APP_ABI := armeabi
--- app/src/main/jni/Application_neon.mk
+++
@@ -1,7 +0,0 @@
-# release
-#APP_CFLAGS := -O3 -mfpu=neon -mfloat-abi=softfp -fPIC -march=armv7-a
-#debug
-APP_CFLAGS := -mfpu=neon -mfloat-abi=softfp -fPIC -march=armv7-a
-#APP_CFLAGS := -marm -march=armv7-a -mfloat-abi=hard -mfpu=neon -ffast-math
-APP_ABI := armeabi-v7a
-APP_PLATFORM := android-8
